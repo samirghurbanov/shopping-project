95d3cf67556376379bfc039052daa587b4c99f76
Samir Qurbanov <qurbanovsamir111@gmail.com>
2023-03-31 04:17:37 -0700

    modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,20 +8,20 @@ import {AiFillInstagram} from 'react-icons/ai'
 export default function Footer() {
   return (
     <div className='footer '>
-      <div className="container mt-5">
-        <div className="row ">
+      <div className="container margintop">
+        <div className="row align-items-center">
           <div className="col-lg-6">
-            <h2>Social</h2>
+            <h2 className='text-dark'>Social</h2>
             <div className='d-flex flex-column '>
-            <p><FaFacebook /> Facebook</p>
-            <p><AiFillInstagram /> Instagram</p>
+            <p ><FaFacebook /> <span className='text-white'>Facebook</span></p>
+            <p><AiFillInstagram /> <span className='text-white'>Instagram</span></p>
             </div>
           </div>
           <div className="col-lg-6">
-            <h2>Contact</h2>
+            <h2 className='text-dark'>Contact</h2>
             <p><BsFillTelephoneFill /> <a className='ms-2' href="tel:+994555727552">0555727552</a></p>
             <p><MdEmail /> <a href="mailto:qurbanovsamir111@gmail.com">qurbanovsamir111@gmail.com</a></p>
-            <p><ImLocation2 /><span className='ms-2'>Sumgait city</span></p>
+            <p><ImLocation2 /><span className='ms-2 text-white'>Sumgait city</span></p>
           </div>
           
         </div>
